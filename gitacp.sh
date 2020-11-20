3d5ac76a9210be85cf9aafaa420f6653b03c67a7
Ibi Hasanli <ibili73@gmail.com>
2020-11-20 04:28:41 -0800

    aliased commit on ubuntu

--- gitacp.sh
+++ gitacp.sh
@@ -7,7 +7,7 @@ for folder in /home/$USER/Documents/Git/*; do
                 git status
                 git add .
                 git commit -m 'Commmited by ~/Documents/Git/Git/gitacp.sh'
-                git push
+                git push origin master
                 cd ..
                 echo "**********************************"
                 echo "**********************************"
